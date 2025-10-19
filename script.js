17c899869f601f322a3c5e5eb0d0f14cbafd8952
moisesfernades16 <moisesfernandes1105@gmail.com>
2025-10-18 22:22:54 -0300

    botão de Editar Adicionado

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// clicar na task escrita  para alterar o texto []  
+// ao clicar no botao de editar, o texto vai para o inputnewtask e depois ele edita a task selecionada []
+
 const localStoragekey = 'to-do-list-ma'
 const input = document.getElementById('inputNewTask')
 
@@ -36,13 +39,20 @@ function showValues() {
     let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
-    for (let i = 0; i < values.length; i++)
-         {
+    for (let i = 0; i < values.length; i++) {
         list.innerHTML += `<li>${values[i].name}<button id="btnOk" onclick='removeItem()'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
   <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+</svg></button> <button id="btnEdit" onclick='editValues(${i})'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
 </svg></button>
 </li>`
     }
+} 
+function editValues(index) {
+    let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
+    let input = document.getElementById('inputNewTask')
+    input.value = values[index].name
+    removeItem(values[index].name)
 }
 function removeItem(data) {
     let values = JSON.parse(localStorage.getItem(localStoragekey) || "[]")
@@ -51,11 +61,13 @@ function removeItem(data) {
     localStorage.setItem(localStoragekey, JSON.stringify(values))
     showValues()
 }
-input.addEventListener('keydown', function(event) {
-   
+input.addEventListener('keydown', function (event) {
+
     if (event.key === 'Enter') {
-        event.preventDefault() 
-        newTask() 
+        event.preventDefault()
+        newTask()
     }
 })
+
+
 showValues()
